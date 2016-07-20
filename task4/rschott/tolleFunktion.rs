42afad3243ad76ed14006c4686b04ec7cefc3b81
Roman Schott <rschott@uni-osnabrueck.de>
2016-07-20 11:20:41 +0200

    improvement of tollefunction.rs

--- task4/rschott/tolleFunktion.rs
+++ task4/rschott/tolleFunktion.rs
@@ -1,9 +1,8 @@
+use std::ops::{Add, Mul};
+
 fn main() {
     println!("Sum: {}, Mul: {}", calculate(2.5, 3.0).0,calculate(2.5, 3.0).1,);
 }
-fn calculate<T: std::ops::Add + std::ops::Mul + Copy>
-    (a: T,
-     b: T)
-     -> (<T as std::ops::Add>::Output, <T as std::ops::Mul>::Output) {
+fn calculate<T: Add + Mul + Copy>(a: T, b: T) -> (<T as Add>::Output, <T as Mul>::Output) {
     (a + b, a * b)
 }
